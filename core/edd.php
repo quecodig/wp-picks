1639a79f5cc491a36cef511ac8de561268a8371e
Qué Código <webmaster@quecodigo.com>
2020-01-27 18:48:06 -0500

    Se desactiva edd

--- core/edd.php
+++ core/edd.php
@@ -29,13 +29,11 @@
 		define( 'EDD_SLUG', 'producto' );
 	}
 
-
 	// Eliminar Campo Cupón de Descuento
 	add_action( 'init', 'apostadores_edd_remove_discount_field' );
 	function apostadores_edd_remove_discount_field() {
 		remove_action( 'edd_checkout_form_top', 'edd_discount_field', -1 );
 	}
-
 
 	// Eliminar Acciones Rápidas Checkout
 	add_filter( 'edd_payment_row_actions', 'apostadores_edd_remove_payment_delete_action', 10, 2 );
@@ -65,7 +63,6 @@
 		}
 		return $translated_text;
 	}
-
 
 	// Icono de Pago Personalizado
 	//add_filter( 'edd_accepted_payment_icons', 'apostadores_edd_payment_icon' );
